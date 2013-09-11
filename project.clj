4993c24606eb51f86c2021a88f5218544486d8df
daphne <daphne.ezer@gmail.com>
2013-09-11 06:53:18 -0400

    init

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "fgrip models facilitated diffusion in promoters."
   :main edu.cam.fgrip.core
   :source-path "src/clj"
-  :java-source-path "src/java"
+  :java-source-path "src"
   :javac-fork "true"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0"]
